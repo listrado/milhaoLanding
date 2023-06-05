b9ec6ad4a960d05d72123d7cd403a592ae3d0eca
listrado <llucas.cardi.rangel@gmail.com>
2023-06-05 00:27:47 -0300

    almost complete project

--- milhao/src/App.js
+++ milhao/src/App.js
@@ -1,43 +1,313 @@
-import logo from './logo.png';
-import foto1 from './milhao.png';
+import foto4 from './arca.png';
+import foto3 from './arca (2).png';
+import foto2 from './pcfolhas.png';
+import foto1 from './milhao (2).png';
+import equity from './equity.png';
+import star1 from './stars1.png';
+import avaliacao2 from './avaliacao2 (2).png';
+import avaliacao1 from './Frame 4094.png';
+import g1 from './Group 481427.png';
+import g2 from './Group 481428.png';
+import g3 from './Group 481429.png';
+import g4 from './Group 481431.png';
+import g5 from './Group 481432.png';
+import b1 from './Frame 4064.png';
+import b2 from './Frame 4066.png';
+import b3 from './Frame 4068.png';
+import b4 from './Frame 4063.png';
+import b5 from './Frame 4058.png';
+import valor from './valor.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container bot">
+        <div className="container bot background">
           <div className="left margin top">
             <img src={foto1} alt="do milhao ao milhao foto" />
-            <p className="container-left f18">
+            <p className="container-54 f18">
               aprenda como encontrar as melhores
               oportunidades do mercado para <b>aumentar,
               proteger e diversificar seu patrimônio</b> e
               finalmente atingir sua <b>liberdade financeira.</b>
             </p>
-            <p className="container-left f12">
+            <p className="container-54 f12">
               faço agora sua matricula
             </p>
             <a href="https://google.com" class="button">fazer matricula</a>          
           </div>
         </div>
-        <div className="container background2 center">
-          <div className="center container-left">
-            <p className="f22">veja as vantagens para quem se matricular nessa turma do mil ao milhao 4.0</p>
+        <div className="container background5 center">
+          <div className="center container-31">
+            <p className="f22">veja as <b>vantagens</b> para quem se matricular nessa turma do mil ao milhao 4.0</p>
           </div>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container background3 center">
+          <div className="center container-24">
+            <img src={equity} alt="equity" />
+          </div>
+          <div className="center container-31">
+            <div>
+              <p className="f22">deconto exclusivo de <b>R$ 3.000,00</b></p>
+              <p className="f18">desconto exclusivo na imersao de negocios do Grupo primo, ministrado pelo Thiago Nigro - imersao presencial</p>
+            </div>
+          </div>
+        </div>
+        <div className="container background2 center">
+          <div className="center padding1">
+            <a href="https://google.com" class="button">faça agora sua matricula</a>          
+          </div>
+        </div>
+        <div className="container background4">
+          <div className="background2 center">
+            <div className="container-24 padding1">
+              <p>ao final das <b>7 semanas</b> voce sera capas de</p>
+            </div>
+          </div>
+          <div className="bot center">
+            <div class="flex-container center margin2">
+              <div className="center box">
+                <p className="f12">Você aprenderá como organizar suas finanças pessoais para ter mais tranquilidade na hora de investir, e controle sobre seus ganhos e gastos</p>
+              </div>
+              <div className="center box">
+                <p className="f12">Entenda o passo a passo com linguagem simples e prática para avaliar, escolher e investir o seu dinheiro com estratégias baseadas no método ARCA.</p>
+              </div>
+              <div className="center box">
+                <p className="f12">Nunca mais você vai ficar longe das oportunidades que o mercado financeiro oferece por não entender como ele funciona. Aqui você terá uma grande mudança de mentalidade e visão.</p>
+              </div>
+              <div className="center box">
+                <p className="f12">Você terá todas as instruções e conhecimento para diversificar o seu investimento e ter uma carteira rentável e promissora.</p>
+              </div>
+              <div className="center box">
+                <p className="f12">Perca o medo de investir por não saber o que fazer por falta de conhecimento e estratégia, eu vou te mostrar como eu penso na hora de investir para que você possa modelar.</p>
+              </div>
+              <div className="center box">
+                <p className="f12">Tudo que você vai aprender vai potencializar e acelerar a sua alavancagem de negócios e patrimônio através do empreendedorismo e investimentos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container background6 center">
+          <div className="center container-24">
+            <p className="f23">oque a mentoria <b>do milhao ao milhao 4.0</b> ira te entregar</p>
+          </div>
+          <div>
+            <div class="vertical-line"></div>
+          </div>
+          <div className="flex-container left container-31">
+            <div>
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+          </div>
+        </div>
+        <div className="container background6 center">
+          <div className="center container-24">
+          </div>
+          <div className="left container-31">
+            <div className="">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+            <div className="">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+            <div className="">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+            <div className="">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+            <div className="">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+            <div className="bot">
+              <p className="f26"><b>semana 01</b> -Diagnóstico estratégico</p>
+              <p className="f12">aprenda o pilar fundamnetal do acúmulo da riquesa: o gerenciamento das suas finanças pessoais.</p>
+              <a href="https://google.com" class="button2">leia mais</a>
+            </div>
+          </div>
+        </div>
+        <div className="container background2 center">
+          <div className="left container-24">
+            <p className="f12 brown">material complementar</p>
+            <h2>apostila digital e mapas mentais</h2>
+            <p className="f18">receba a apostila completa e todos os mapas mentais <b>do mil ao milhao 4.0</b></p>
+            <a href="https://google.com" class="button">faça agora sua matricula</a>
+            <p className="f12 brown">disponivel em todos os dispositivos</p>
+          </div>
+          <div className="padding2 container-31 margin2">
+            <div>
+              <img src={foto2} alt="foto2" />
+            </div>
+          </div>
+        </div>
+        <div className="container background7 center">
+          <div className="left container-24">
+            <p className="f26">o que e o metodo <b>arca</b></p>
+            <p className="f12">o metodo ARCA é um modelo de alocaçao ensinado pelo Thiago Nigro, O Primo Rico, que permite ter ganhos significativos em seus investimentos, ao mesmo tempo que diminui as perdas nos momentos de stress. testado e validado em + de 7 países</p>
+            <img src={foto3} alt="metodo" />
+          </div>
+          <div className="padding2 container-31 margin2">
+            <div>
+              <img src={foto4} alt="arca" />
+            </div>
+          </div>
+        </div>
+        <div className="background8 container">
+          <div className="top">
+            <img src={star1} alt="star" />
+            <p className="f22 margin0"><b>Transformaçao Real!</b></p>
+          </div>
+          <div class="flex-container2 background9">
+            <div className="box2">
+              <div className="center margin3">
+                <h1>98,</h1>
+                <h2>30%</h2>
+              </div>
+              <p className="f12">Dos alunos consideram que investem melhor depois de ter feito a mentoria.</p>
+            </div>
+            <div class="separator"></div>
+            <div className="box2">
+              <div className="center margin3">
+                <h1>98</h1>
+                <h2>%</h2>
+              </div>
+              <p className="f12">Dos alunos se sentem confiantes em investir seu dinheiro.</p>
+            </div>
+            <div class="separator"></div>
+            <div className="box2">
+              <div className="center margin3">
+                <h1>93,</h1>
+                <h2>20%</h2>
+              </div>
+              <p className="f12">Dos alunos consideram que passaram a gastar melhor.</p>
+            </div>
+            <div class="separator"></div>
+            <div className="box2">
+              <div className="center margin3">
+                <h1>66</h1>
+                <h2>%</h2>
+              </div>
+              <p className="f12">Dos alunos consideram que passaram a ganhar mais SOMENTE EM 3 MESES</p>
+            </div>
+          </div>
+          <div className="bot center">
+            <div className="box3">
+              <img src={avaliacao1} alt="star" />
+              <img src={avaliacao2} alt="star" />
+            </div>
+            <div className="container box4 margin1">
+              <p className="f18"><b>O que é NPS?</b></p>
+              <p className="f18">É uma metodologia de satisfalção desenvolvida para avaliar o grau de fidelidade dos clientes de uma empresa</p>
+            </div>
+          </div>
+          <div className="bot">
+            <a href="https://google.com" class="button">faça agora sua matricula</a>
+          </div>
+        </div>
+        <div className="background2 container top">
+          <div className="flex-container center">
+            <div className="container-24 white left">
+              <p>Ao entrar no Mil ao Milhao 4.0 você <b>recebera:</b></p>
+            </div>
+            <div className="box5">
+            <img src={g5} alt="star" className="box2"/>
+            </div>
+          </div>
+          <div className="flex-container center">
+            <div className="container-24 white">
+            </div>
+            <div className="box5">
+              <img src={g2} alt="star" className="box2"/>
+              <img src={g3} alt="star" className="box2"/>
+              <img src={g4} alt="star" className="box2"/>
+              <img src={g1} alt="star" className="box2"/>
+            </div>
+          </div>
+        </div>
+        <div className="background10 container bot">
+          <div className="container ">
+            <div className=" center">
+              <div className="black padding1">
+                <p>5 <b>Super</b> Bônus</p>
+              </div>
+            </div>
+            <div className="bot center">
+              <div class="flex-container center margin2">
+                <div className="center box6">
+                  <img src={b1} alt="star" className="bonus"/>
+                </div>
+                <div className="center box6">
+                  <img src={b2} alt="star" className="bonus"/>
+                </div>
+                <div className="center box6">
+                  <img src={b3} alt="star" className="bonus"/>
+                </div>
+                <div className="center box6">
+                  <img src={b4} alt="star" className="bonus"/>
+                </div>
+                <div className="center box6">
+                  <img src={b5} alt="star" className="bonus"/>
+                </div>
+              </div>
+            </div>
+            <div  className="black f26">
+              <p>Garanta tudo isso <b>por apenas</b> 12x de</p>
+              <div className="center margin4">
+                <p>R$</p>
+                <p className="f80">292</p>
+                <p>,00</p>
+              </div>
+              <p>ou a vista por R$2997</p>
+              <a href="https://google.com" class="button">faça agora sua matricula</a>
+            </div>
+          </div>
+        </div>
+        <div className="container background6 center">
+          <div className="flex-container3 center">
+            <div className="padding1">
+              <div className="box2 black">
+                <p className="f32"><b>Veja tudo</b> que você terá acesso hoje</p>
+              </div>
+              <div className="box2 left f12">
+                <p>Desconto exclusivo para o Equity+ (Imersão de Negócios)</p>
+              </div>
+            </div>
+            <div>
+              <div className="box2">
+                <img src={valor} alt="valor"/>
+              </div>
+              <div className="padding3">
+                <a href="https://google.com" class="button">faça agora sua matricula</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          
+        </div>
+        <div className="">
+          
+        </div>
+        <div className="">
+          
+        </div>
+        <div className="">
+          
+        </div>
+        asdaw
       </header>
     </div>
   );
